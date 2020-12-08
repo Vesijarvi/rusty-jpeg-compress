7ab359866ea8d98d8e0b5a27bc2dba3381df3510
yp.pan <pan.yiping.fi@gmail.com>
2020-12-08 21:04:51 +0800

    Add image.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+// pub mod compress;
+// use compress::huffman;
+
+mod image;
+
+use image::Image;
+
+extern crate clap;
+use clap::{App, Arg};
+use std::fs;
+use std::io::Read;
+use crate::fs::File;
+
+
+fn get_file_as_byte_vec(filename: &String)->Vec<u8> {
+    let error_msg = "Error reading file: ".to_string() + filename;
+    let mut f = File::open(&filename).expect(&error_msg);
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+    
+    buffer
+}
+
+fn main(){
+    let matches = App::new("JPEG compress")
+        .arg(
+            Arg::with_name("compress")
+                .short("c")
+                .long("compress")
+                .value_name("FILE")
+                .takes_value(true),
+        )
+        .get_matches();
+ 
+    if let Some(file) = matches.value_of("compress"){
+        let byte_stream: Vec<u8> = get_file_as_byte_vec(&file.to_string());
+        println!("{:?}",byte_stream);
+        // let compressed_data = huffman::compress(&byte_stream);
+
+
+        // let output_file = file.to_string() + ".cmp";
+        // let error_msg = "Error writing file: ".to_string();
+	// fs::write(output_file, compressed_data).expect(&error_msg);
+    }
 }
