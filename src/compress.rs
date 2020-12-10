100cf4567a8b6254ef3e2c4e7876048b8d8f430d
YiPing <pan.yiping.fi@gmail.com>
2020-12-10 21:33:18 +0800

    Small edits for better readibility

--- src/compress.rs
+++ src/compress.rs
@@ -3,7 +3,10 @@
 // Read steps
 // [v] 1. Get in u8 string 
 // [v] 2. Convert into YCbCr
-// [ ] 3. Blockize 
+// [v] 3. Blockize 
+// [ ] 4. DCT
+// [ ] 5. quantilize
+// [ ]
 pub mod jpeg {
     use std::default::Default;
 
@@ -36,12 +39,9 @@ pub mod jpeg {
         [ 21, 34, 37, 47, 50, 56, 59, 61 ],
         [ 35, 36, 48, 49, 57, 58, 62, 63 ]
     ];
-
-    
     // Transfer byte stream to Color 
     fn read_vec8_to_color(stream_vec: &Vec<u8>) -> Vec<Color> {
         let mut color_vec = Vec::new();
-
         let mut r = 0;
         let mut g = 0;
         let mut b = 0;
@@ -54,7 +54,7 @@ pub mod jpeg {
                 2 => { 
                     b = *c; 
                     // convert RGB to YCbCr
-                    let y:f32 = 0.299 * r as f32 + 0.587 * g as f32 + 0.114 * b as f32;
+                    let y: f32 = 0.299 * r as f32 + 0.587 * g as f32 + 0.114 * b as f32;
                     let cb: f32 = -0.168 * r as f32  - 0.331 * g as f32 + 0.499 * b as f32;
                     let cr: f32 = 0.5 * r as f32 - 0.419 * g as f32 - 0.081 * b as f32;
                     let myColor = Color::YCbCr(y,cb,cr);
@@ -67,8 +67,9 @@ pub mod jpeg {
         color_vec
     }
 
-    fn read_color_to_mcus(color_vec: Vec<Color>) -> Vec<Vec<Block>>  {
+    fn read_color_to_mcus(color_vec: &Vec<Color>) -> Vec<Vec<Block>>  {
         // still can make improvement to simplify code
+        // lets just leave it for now
         let mut Y_plane = Vec::new();
         let mut Cb_plane = Vec::new();
         let mut Cr_plane = Vec::new();
@@ -90,16 +91,20 @@ pub mod jpeg {
         }
         for y in 0..256 {
             for x in 0..256 {
-                bitplanes[1][(x / 8) + 4 * (y / 8)][x % 8][y % 8] = Y_plane[x + 256 * y];
+                bitplanes[1][(x / 8) + 4 * (y / 8)][x % 8][y % 8] = Cb_plane[x + 256 * y];
             }
         }
         for y in 0..256 {
             for x in 0..256 {
-                bitplanes[2][(x / 8) + 4 * (y / 8)][x % 8][y % 8] = Y_plane[x + 256 * y];
+                bitplanes[2][(x / 8) + 4 * (y / 8)][x % 8][y % 8] = Cr_plane[x + 256 * y];
             }
         }
         bitplanes
     }
+    /*
+    fn dct(in_mcus: &Vec<Vec<Block>>) -> Vec<Vec<Block>> {
+
+    }*/
     pub fn compress(stream_vec: &Vec<u8>) {
         let colorYCbCr = read_vec8_to_color(&stream_vec);
         // println!("{:?}",myColor);
